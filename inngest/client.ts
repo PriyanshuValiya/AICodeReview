c07f93f792385d14571eb1ac78106f7c21cdbf9b
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2025-12-18 00:21:33 +0530

    add inngest prod

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,3 +1,10 @@
+// import { Inngest } from "inngest";
+
+// export const inngest = new Inngest({ id: "ai-code-reviewer" });
+
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "ai-code-reviewer" });
+export const inngest = new Inngest({
+  id: "ai-code-reviewer",
+  eventKey: process.env.INNGEST_EVENT_KEY!,
+});
